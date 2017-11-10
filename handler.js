f2f5e2711120a5620e04e0f2c3364a1056edc132
Michel Grootjans <michel.grootjans@gmail.com>
2017-11-10 14:11:02 +0100

    removed get_votes_2

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports.get_votes_2 = (event, context, callback) => {
+module.exports.get_votes = (event, context, callback) => {
 
   // Load the AWS SDK for Node.js
   var AWS = require('aws-sdk');
@@ -49,21 +49,8 @@ function onScan(err, data) {
         
     }
 }
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
-
-module.exports.get_votes = (event, context, callback) => {
-  console.log('get_votes was called')
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({votes: {yes: 3, no: 2}}),
-  };
-  callback(null, response);
-};
 
 module.exports.post_votes = (event, context, callback) => {
   console.log('get_votes was called')
